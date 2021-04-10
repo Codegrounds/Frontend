1fa0de39b240c52e714eb74b0f78026f409d0e0f
Peter Downey <peter.d.downey@gmail.com>
2021-04-10 13:15:38 -0400

    added login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import LessonPage from "./LessonPage";
 import OverviewPage from "./OverviewPage";
+import LoginPage from "./LoginPage";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Router>
           <Switch>
             <Route path ='/' exact component={OverviewPage}/>
+            <Route path ='/login' exact component={LoginPage}/>
             <Route path ='/lessons' component={LessonPage}/>
           </Switch>
       </Router>

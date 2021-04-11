713a7532c313f9778453851d09a20582e7b33464
Peter Downey <peter.d.downey@gmail.com>
2021-04-11 10:27:25 -0400

    its better than nothing
    im tired as fuck

--- src/pages/LoginFields.js
+++ src/pages/LoginFields.js
@@ -3,56 +3,59 @@ import { useHistory } from 'react-router-dom';
 import 'codegrounds/styles/App.css';
 
 export function LoginFields() {
-    let history = useHistory();
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+	let history = useHistory();
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
-    const handleSubmit = async (event) => {
-        event.preventDefault()
+	const handleSubmit = async (event) => {
+		event.preventDefault()
 
-        const loginResult = await fetch('https://codegrounds.atale.me/v1/authentication/login', {
-            method: 'POST',
-            credentials: 'include',
-            body: JSON.stringify({
-                username: username,
-                password: password
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
+		const loginResult = await fetch('https://codegrounds.atale.me/v1/authentication/login', {
+			method: 'POST',
+			credentials: 'include',
+			body: JSON.stringify({
+				username: username,
+				password: password
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
 
-        console.log(loginResult)
-        console.log(await loginResult.json())
+		console.log(loginResult)
+		console.log(await loginResult.json())
 
-        const testResult = await fetch('https://codegrounds.atale.me/v1/authentication/test', {
-            method: 'POST',
-            credentials: 'include'
-        })
+		const testResult = await fetch('https://codegrounds.atale.me/v1/authentication/test', {
+			method: 'POST',
+			credentials: 'include'
+		})
 
-        console.log(testResult)
-        console.log(await testResult.json())
+		console.log(testResult)
+		console.log(await testResult.json())
 
-        if (testResult.status === 200) {
-            history.push('/overview')
-        }
-    }
+		if (testResult.status === 200) {
+			history.push('/overview')
+		}
+	}
 
-    return (
-        <div className="LoginPage">
+	return (
+		<div className="LoginPage">
 
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Username:
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                </label>
-                <label>
-                    Password:
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+			<div className="LoginPageContainer">
+				<form onSubmit={handleSubmit}>
+					<div>
+						Username:
+                  		<input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+					</div>
+					<div>
+						Password:
+                    	<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+					</div>
 
-        </div>
-    );
+					<input type="submit" value="Submit" style={{ width: '50%', height: '20%' }} />
+				</form>
+
+			</div>
+		</div>
+	);
 }
